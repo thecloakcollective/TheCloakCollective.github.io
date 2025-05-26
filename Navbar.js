2c26648b6dac1840a47eaa1fec7796800660b019
Babaruser37 <christianumbertogarcia@gmail.com>
2025-05-26 15:57:44 -0400

    Fixed broken logic, cleaned up file heirarchy , higher resolution images
    The carousel was no longer working. Remade a new one using slider.js using the same timings and style. Did remove the hover effect as it was redundant in my opinion. Also re-sized all the images to make them higher resolution.All the files used to be under 1 direcotry with .html but I moved them all to directories and changed them to index.html. Makes everything look cleaner and makes the url cleaner as well.

---
+++ Navbar.js
@@ -0,0 +1,31 @@
+document.addEventListener("DOMContentLoaded", function () {
+  const navbarHTML = `
+    <nav>
+      <div class="navbar">
+        <div class="Ccontainer nav-container">
+            <input class="checkbox" type="checkbox" name="" id="" />
+            <div class="hamburger-lines">
+              <span class="line line1"></span>
+              <span class="line line2"></span>
+              <span class="line line3"></span>
+            </div>  
+          <div class="logo">
+            <a href="/index.html"><img src="/images/Logo.PNG" alt="Logo"></a>
+          </div>
+          <div class="menu-items">
+            <li><a href="/index.html">Home</a></li>
+            <li><a href="/bio">About</a></li>
+            <li><a href="/Portfolio">Portfolio</a></li>
+            <li><a href="/Contact">Contact</a></li>
+          </div>
+        </div>
+      </div>
+    </nav>
+  `;
+
+  // Inject into a placeholder element
+  const navbarContainer = document.getElementById("navbar-placeholder");
+  if (navbarContainer) {
+    navbarContainer.innerHTML = navbarHTML;
+  }
+});
